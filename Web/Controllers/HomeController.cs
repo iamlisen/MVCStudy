69ec27b0f4c3fcf2806398c0a2d586a15c11e610
iamlisen <iamlisen@163.com>
2017-04-19 22:14:46 +0800

    1

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -5,7 +5,6 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Ajax;
 
-
 namespace Web.Controllers
 {
 	public class HomeController : Controller
@@ -15,27 +14,6 @@ namespace Web.Controllers
 			ViewBag.Greeting = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
 			ViewBag.Title = "首页";
 			return View();
-		}
-
-		[HttpGet]
-		public ActionResult RsvpForm(){
-			return View();
-		}
-
-		[HttpPost]
-		public ViewResult RsvpForm(GuestResponse response){
-			if (ModelState.IsValid)
-			{
-				ViewBag.Title = "谢谢";
-				return View(response);
-			}else{
-				return View();
-			}
-		}
-
-		[HttpPost]
-		public ViewResult Thanks(GuestResponse response){
-			return View(response);
 		}
 	}
 }

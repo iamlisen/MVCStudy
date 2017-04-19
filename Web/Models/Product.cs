d42e09d0229821bc572ad536fd5ee1ca5f67c770
iamlisen <iamlisen@163.com>
2017-04-19 22:14:12 +0800

    1

---
+++ Web/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Web
+{
+	public class Product
+	{
+		public Product()
+		{
+		}
+
+		public int ProductID { get; set; }
+		public string Name { get; set;}
+		public string Description { get; set; }
+		public decimal Price { get; set; }
+		public string Category { get; set; }
+	}
+}

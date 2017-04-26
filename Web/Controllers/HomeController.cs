e1f2f253abeabbbba0f3b2daaa99bf1f39e251a1
iamlisen <iamlisen@163.com>
2017-04-26 19:14:38 +0800

    1

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -9,10 +9,31 @@ namespace Web.Controllers
 {
 	public class HomeController : Controller
 	{
+		Product myProduct = new Product
+		{
+			ProductID = 1,
+			Name="Kayak",
+			Description="A boat for a person",
+			Category="Watersports",
+			Price=275M
+		};
+
 		public ActionResult Index()
 		{
-			ViewBag.Greeting = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
 			ViewBag.Title = "首页";
+			return View(myProduct);
+		}
+
+		public ActionResult NameAndPrice(){
+			ViewBag.Title = "名称&价格";
+			return View(myProduct);
+		}
+
+		public ActionResult DemoExpression(){
+			ViewBag.ProductCount = 1;
+			ViewBag.ExpressShip = true;
+			ViewBag.ApplyDiscount = false;
+			ViewBag.Supplier = null;
 			return View();
 		}
 	}
